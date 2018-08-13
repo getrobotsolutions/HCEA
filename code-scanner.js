6ec22936291155d0be326ead85233ccbaaa465b4
tamoorkayani <tamoorkayani@gmail.com>
2018-08-13 18:19:10 +0500

    Scanner update
    Scanner update

--- code-scanner.js
+++ code-scanner.js
@@ -1,33 +1,20 @@
 
-var badgeNumber=1234567;
-
 function CheckVal(){
     var bar_code = $("#bar_code").val(); 
     var len = bar_code.length;
-                      var parts = bar_code.split("^");
-                      var regId=parts[0];
-                      var firstName=parts[1];
-                      var lastName=parts[2];
-                      var companyName=parts[3];
+    var parts = bar_code.split("^");
+    var regId=parts[0];
+    var firstName=parts[1];
+    var lastName=parts[2];
+    var companyName=parts[3];
   if(regId==null || firstName==null || lastName==null || companyName== null){
-                        alert("Please Scan badge again");
-                          location.reload();
+    alert("Please Scan badge again");
+    location.reload();
   }
   else{
-                      window.external.PlaySpeech("welcome" +firstName + " " + lastName +", welcome to our booth."); 
-                        /*alert("Reg ID: " + parts[0]);
-                        alert("First Name: " + parts[1]);
-                        alert("Last Name: " + parts[2]);
-                        alert("Company Name:" + parts[3]);
-                                                                                      
-              var dNow = new Date();
-              var month = dNow.getMonth()+1;
-              var day = dNow.getDate();
-              var year = dNow.getFullYear();
-              var hour = dNow.getHours();
-              var min = dNow.getMinutes();
-              */
-            var dataString =  { 'RegId' : regId, 'FirstName' : firstName,  'LastName' : lastName, 'CompanyName' : companyName};
+      //window.external.PlaySpeech("welcome" +firstName + " " + lastName +", welcome to our booth."); 
+                        
+            /*var dataString =  { 'RegId' : regId, 'FirstName' : firstName,  'LastName' : lastName, 'CompanyName' : companyName};
             $.ajax({
               url:"https://robotaisolutions.com/amgen/hceaApi.php",
               type:"get",
@@ -44,50 +31,53 @@ function CheckVal(){
                   }
                   //window.external.PlaySpeech("welcome" +firstName + " " + lastName +"to the CDS booth");
                 }
-            });
+            });*/
   }      
 }
-  $(function () {
-    $('#bar_code').codeScanner();
-    var bar_code = $("#bar_code"); 
-    
-});              
-  $(document).ready(function() {
-      $("#bar_code").focus();
-      $(".second-block").click(function(){
-        location.href = "maincontents.htm";
-      });
-  });
-  $( function() {
-                  $('.submit').click(function(e) {
-                      e.preventDefault();
-                     
-                      var bar_code = $("#bar_code").val(); 
-                  //    console.log(bar_code);
-                    
-                    if(bar_code.length>0){
-                       
-                        CheckVal();
-                        //alert(bar_code);
-                        //alert(bar_code.length); 
-                        location.href = "maincontents.htm";
+              
+$(document).ready(function() {
+  $("#bar_code").focus();
 
-                    }
-                    else{
-                      window.setTimeout(function() {
-                            var bar_code = $("#bar_code").val();
-                            CheckVal(bar_code);
-                        }, 5000);
-                    }
+  $(".second-block").click(function(){
+    location.href = "maincontents.htm";
+  });
+
+  $('#bar_code').codeScanner();
+  var bar_code = $("#bar_code"); 
+    
+  $("#bar_code").focus();
+  $(".second-block").click(function(){
+    location.href = "maincontents.htm";
+  });
+
+  $('.submit').click(function(e) {
+    e.preventDefault();
+                     
+    var bar_code = $("#bar_code").val(); 
+    //    console.log(bar_code);
+                    
+    if(bar_code.length>0){
+                    
+      CheckVal();
+                         
+      location.href = "maincontents.htm";
+
+    }
+    else{
+      window.setTimeout(function() {
+        var bar_code = $("#bar_code").val();
+        //CheckVal(bar_code);
+      }, 5000);
+    }
                                          
-                  });
+  });
                   
-                    setTimeout(function() {
-                      $('#submit').trigger('click');
-                      }, 5000);
+  setTimeout(function() {
+    $('#submit').trigger('click');
+  }, 5000);
 
                  
-        });
+});
 
   function OnJoystickControlled(strPara){
     var btn_info = strPara.split(',')[4];
@@ -117,4 +107,4 @@ function CheckVal(){
         window.external.PlaySpeech("Ha, ha.  That tickles.  Good luck on winning a great prize.");
         
     }
-}
+  }
